4a5a966849155c5c5e9feb5a1f30bfdd962a190e
lordlamee <gabrielolamideakorede@gmail.com>
2020-08-18 17:58:16 +0100

    added mobile client

---
+++ ecx_learn_mobile/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EcxLearn());
+}
+
+class EcxLearn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Well\n this is the starting project'),
+      ),
+    );
+  }
+}
